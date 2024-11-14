7475a16f55243cf4b10ed12b3d52e4e7b6c3a1a6
Aleksander Obrzut <EMAIL>
2024-11-14 08:27:10 +0100

    poprawki

--- znajdz.php
+++ znajdz.php
@@ -28,10 +28,24 @@
         
         <form method="POST" action="znajdz.php">
             <label for="miasto">Podaj nazwę miasta:</label>
+            <input type="text" name="miasto">
             <input type="submit" value="SPRAWDŹ">
         </form>
-        
-        <div id="wynik"></div>
+        <div id="wynik">
+        <?php
+            $polaczenie = mysqli_connect('localhost', 'root', '','kwiaciarnia');
+
+            $zapytanie = "SELECT * FROM kwiaciarnie WHERE miasto = '".$_POST['miasto']."'";
+
+            $wynik = mysqli_query($polaczenie, $zapytanie);
+                if(mysqli_num_rows($wynik) > 0) {
+                    echo "<h3>" . $_POST['nazwa'] . ", " . $_POST['ulica'] . "</h3>";
+
+                } else {
+                    echo "Brak kwiaciarni w podanym mieście";
+                }
+        ?>
+        </div>
     </main>
     <footer>
         <p>Stronę opracował: 0000000000000</p>
